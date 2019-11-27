99a763078d368438342a3b6a78db9654aa04e9d4
Daniel Petersen <dlfp16@student.aau.dk>
2019-11-27 14:14:09 +0100

    day 2

--- MultiModal/MultiModall/Assets/DetectTouch.cs
+++ MultiModal/MultiModall/Assets/DetectTouch.cs
@@ -4,34 +4,150 @@ using UnityEngine;
 
 public class DetectTouch : MonoBehaviour
 {
+
+    public bool detected = false;
+    public bool detected2 = false;
     public AudioSource correct, wrong;
     public GameObject Player;
+    private IEnumerator coroutine;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coroutine = WaitAndNext(0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Player.GetComponent<modeSelect>().test5Wrong);
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Hand"))
-        {
-            if (this.gameObject == Player.GetComponent<modeSelect>().cubes[Player.GetComponent<modeSelect>().current])
+        
+            if (other.gameObject.CompareTag("Hand"))
             {
-                Player.GetComponent<modeSelect>().counter = Player.GetComponent<modeSelect>().counter + 1;
+            if (Player.GetComponent<modeSelect>().mode == 5)
+            {
+                if (detected == false)
+                {
+                    detected = true;
+                    StartCoroutine(coroutine);
+                    if (Player.GetComponent<modeSelect>().mode == 3 || Player.GetComponent<modeSelect>().mode == 4 || Player.GetComponent<modeSelect>().mode == 5)
+                    {
+                        vibrationManager.singleton.triggerVibration(0, 0, 0);
+                    }
+                    if (this.gameObject == Player.GetComponent<modeSelect>().cubes[Player.GetComponent<modeSelect>().test51])
+                    {
+                        Player.GetComponent<modeSelect>().test5CorrectVisual = Player.GetComponent<modeSelect>().test5CorrectVisual + 1;
+                    }
+                    else if (this.gameObject == Player.GetComponent<modeSelect>().cubes[Player.GetComponent<modeSelect>().test52])
+                    {
+                        Player.GetComponent<modeSelect>().test5CorrectAudio = Player.GetComponent<modeSelect>().test5CorrectAudio + 1;
+                    }
+                    else if (this.gameObject == Player.GetComponent<modeSelect>().cubes[Player.GetComponent<modeSelect>().test53])
+                    {
+                        Player.GetComponent<modeSelect>().test5CorrectHaptic = Player.GetComponent<modeSelect>().test5CorrectHaptic + 1;
+                    }
+                    else
+                    {
+                        Player.GetComponent<modeSelect>().test5Wrong = Player.GetComponent<modeSelect>().test5Wrong + 1;
+                    }
+                }
                 correct.Play();
-
+                if (Player.GetComponent<modeSelect>().levelIsStarted == false)
+                {
+                    Player.GetComponent<modeSelect>().levelIsStarted = true;
+                }
             }
-            else
+            if (Player.GetComponent<modeSelect>().mode <= 4)
             {
-                wrong.Play();
+                if (this.gameObject == Player.GetComponent<modeSelect>().cubes[Player.GetComponent<modeSelect>().current])
+                {
+
+                    if (detected == false)
+                    {
+                        detected = true;
+                        StartCoroutine(coroutine);
+                        if (Player.GetComponent<modeSelect>().mode == 3 || Player.GetComponent<modeSelect>().mode == 4 || Player.GetComponent<modeSelect>().mode == 5)
+                        {
+                            vibrationManager.singleton.triggerVibration(0, 0, 0);
+                        }
+                        //shitty if statements for score system
+                        if (Player.GetComponent<modeSelect>().mode == 1)
+                        {
+                            Player.GetComponent<modeSelect>().test1Correct = Player.GetComponent<modeSelect>().test1Correct + 1;
+                        }
+                        if (Player.GetComponent<modeSelect>().mode == 2)
+                        {
+                            Player.GetComponent<modeSelect>().test2Correct = Player.GetComponent<modeSelect>().test2Correct + 1;
+                        }
+                        if (Player.GetComponent<modeSelect>().mode == 3)
+                        {
+                            Player.GetComponent<modeSelect>().test3Correct = Player.GetComponent<modeSelect>().test3Correct + 1;
+                        }
+                        if (Player.GetComponent<modeSelect>().mode == 4)
+                        {
+                            Player.GetComponent<modeSelect>().test4Correct = Player.GetComponent<modeSelect>().test4Correct + 1;
+                        }
+
+
+                    }
+                    correct.Play();
+                    if (Player.GetComponent<modeSelect>().levelIsStarted == false)
+                    {
+                        Player.GetComponent<modeSelect>().levelIsStarted = true;
+                    }
+                }
+
+
+                else
+                {
+
+                    if (detected == false)
+                    {
+
+                        StartCoroutine(coroutine);
+
+
+                        //shitty if statements for score system
+                        if (Player.GetComponent<modeSelect>().mode == 1)
+                        {
+                            Player.GetComponent<modeSelect>().test1Wrong = Player.GetComponent<modeSelect>().test1Wrong + 1;
+                        }
+                        if (Player.GetComponent<modeSelect>().mode == 2)
+                        {
+                            Player.GetComponent<modeSelect>().test2Wrong = Player.GetComponent<modeSelect>().test2Wrong + 1;
+                        }
+                        if (Player.GetComponent<modeSelect>().mode == 3)
+                        {
+                            Player.GetComponent<modeSelect>().test3Wrong = Player.GetComponent<modeSelect>().test3Wrong + 1;
+                        }
+                        if (Player.GetComponent<modeSelect>().mode == 4)
+                        {
+                            Player.GetComponent<modeSelect>().test4Wrong = Player.GetComponent<modeSelect>().test4Wrong + 1;
+                        }
+
+
+                    }
+
+                    correct.Play();
+                }
             }
+            }
+        
+    }
+
+    private IEnumerator WaitAndNext(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Player.GetComponent<modeSelect>().counter = Player.GetComponent<modeSelect>().counter + 1;
+            detected = false;
+            StopCoroutine(coroutine);
+            
+            
         }
     }
 }
--- MultiModal/MultiModall/Assets/modeSelect.cs
+++ MultiModal/MultiModall/Assets/modeSelect.cs
@@ -4,55 +4,94 @@ using UnityEngine;
 using TMPro;
 public class modeSelect : MonoBehaviour
 {
+    public GameObject rightHand;
 
-
+    //countdown variables
+    public float timeleft = 30.0f;
+    public bool levelIsStarted = false;
 
     public GameObject audiozone;
     public Material select, standard;
     public GameObject cube1, cube2, cube3, cube4, cube5, cube6;
 
+    //variables for saving
+    public int test1Wrong = 0;
+    public int test1Correct = 0;
+
+    public int test2Wrong = 0;
+    public int test2Correct = 0;
+
+    public int test3Wrong = 0;
+    public int test3Correct = 0;
+
+    public int test4Wrong = 0;
+    public int test4Correct = 0;
+
+    public int test5Wrong = 0;
+    public int test5Correct = 0;
+    public int test5CorrectAudio = 0;
+    public int test5CorrectVisual = 0;
+    public int test5CorrectHaptic = 0; 
 
     public GameObject[] cubes;
     public int counter = 0;
     public int current = 0;
-    int[] array1 = new int[] { 0, 2, 4, 5, 1,2,3,0,5,1,5,1,3,2,3,5,3,1,5,2,4,5 };
+    int[] array1 = new int[] { 0, 2, 4, 5, 1, 2, 3, 0, 5, 1, 5, 1, 3, 2, 3, 5, 3, 1, 5, 2, 4, 5, 3, 0, 2, 3, 5, 1, 4, 2, 0, 5, 3, 0, 2, 3, 1, 4, 0, 5, 3, 4, 2, 0, 5 };
+    int[] array2 = new int[] { 2, 4, 0, 1, 3, 4, 5, 2, 1, 3, 1, 3, 5, 4, 5, 1, 5, 3, 1, 4, 0, 1, 5, 2, 4, 5, 1, 3, 0, 4, 2, 1, 5, 2, 4, 5, 3, 0, 2, 1, 5, 0, 4, 2, 1 };
+    int[] array3 = new int[] { 1, 5, 3, 2, 0, 4, 0, 3, 0, 5, 2, 3, 0, 1, 5, 3, 4, 2, 0, 1, 5, 2, 3, 0, 1, 3, 5, 3, 2, 0, 1, 5, 1, 3, 2, 1, 5, 4, 0, 4, 0, 2, 0, 1, 0 };
+    int[] array4 = new int[] { 1, 2, 0, 1, 3, 5, 4, 2, 0, 1, 0, 5, 0, 2, 5, 3, 4, 0, 2, 3, 5, 0, 5, 1, 3, 5, 4, 3, 1, 0, 2, 5, 1, 2, 5, 3, 5, 4, 1, 3, 0, 4, 1, 3, 0, 1, 2, 0, 1, 3, 5, 4, 2, 0, 1, 0, 5, 0, 2, 5, 3, 4, 0, 2, 3, 5, 0, 5, 1, 3, 5, 4, 3, 1, 0, 2, 5, 1, 2, 5, 3, 5, 4, 1, 3, 0, 4, 1, 3, 0 };
     public GameObject text;
     //the mode is the sense we are testing. 1 = visual, 2 = audio, 3 = haptic. 
     //Is set manually in the inspector
     public int mode = 0;
+
+
+
+    public int test51 = 0;
+    public int test52 = 0;
+    public int test53 = 0;
     // Start is called before the first frame update
     void Start()
     {
-        if (mode == 1)
-        {
-            text.GetComponent<TextMeshPro>().text = "Currently testing: \n Visual";
-        }
-        else if (mode == 2)
-        {
-            text.GetComponent<TextMeshPro>().text = "Currently testing: \n Audio";
-        }
-        else if (mode == 3)
-        {
-            text.GetComponent<TextMeshPro>().text = "Currently testing: \n Haptic";
-        }
+        timeleft = 30.0f;
 
-        if (mode == 2)
-        {
-            audiozone.gameObject.SetActive(true);
-        }
-        else
-        {
-            audiozone.gameObject.SetActive(false);
-        }
-
-       cubes = new GameObject[] { cube1, cube2, cube3, cube4, cube5, cube6 };
+    cubes = new GameObject[] { cube1, cube2, cube3, cube4, cube5, cube6 };
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(timeleft);
+        modesetup();
+
         testing();
-        current = array1[counter];
+        if (mode == 1)
+        {
+            current = array1[counter];
+        }else if (mode == 2)
+        {
+            current = array2[counter];
+        }
+        else if (mode == 3)
+        {
+            current = array3[counter];
+        }
+        else if (mode == 4)
+        {
+            current = array4[counter];
+        }
+        else if (mode == 5)
+        {
+            test51 = array1[counter];
+            test52 = array2[counter];
+            test53 = array3[counter];
+        }
+
+        if (levelIsStarted == true)
+        {
+            countDown();
+        }
+
     }
 
     public void testing()
@@ -68,7 +107,60 @@ public class modeSelect : MonoBehaviour
         }
         if(mode == 2)
         {
+            for (int i = 0; i < 6; i++)
+            {
+                cubes[i].GetComponent<MeshRenderer>().material = standard;
+            }
             audiozone.transform.position = cubes[current].transform.position;
+        }
+
+
+        if (mode == 3)
+        {
+            
+            float dist = Vector3.Distance(cubes[current].transform.position, rightHand.transform.position);
+            float nonliniar = 2.0f - dist;
+
+            float mapped = nonliniar / 4.0f * 255;
+
+            
+            vibrationManager.singleton.triggerVibration(40, 2, Mathf.RoundToInt(mapped));
+        }
+
+        if (mode == 4)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                cubes[i].GetComponent<MeshRenderer>().material = standard;
+            }
+            cubes[current].GetComponent<MeshRenderer>().material = select;
+
+            audiozone.transform.position = cubes[current].transform.position;
+
+            float dist = Vector3.Distance(cubes[current].transform.position, rightHand.transform.position);
+            float nonliniar = 2.0f - dist;
+
+            float mapped = nonliniar / 4.0f * 255;
+
+
+            vibrationManager.singleton.triggerVibration(60, 2, Mathf.RoundToInt(mapped));
+        }
+
+        if (mode == 5)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                cubes[i].GetComponent<MeshRenderer>().material = standard;
+            }
+            cubes[test51].GetComponent<MeshRenderer>().material = select;
+
+            audiozone.transform.position = cubes[test52].transform.position;
+
+            float dist = Vector3.Distance(cubes[test53].transform.position, rightHand.transform.position);
+            float nonliniar = 2.0f - dist;
+
+            float mapped = nonliniar / 4.0f * 255;
+            vibrationManager.singleton.triggerVibration(60, 2, Mathf.RoundToInt(mapped));
         }
   
     }
@@ -77,5 +169,65 @@ public class modeSelect : MonoBehaviour
     private void nextIte()
     {
         counter = counter + 1;
+    }
+
+
+    public void countDown()
+    {
+        if (timeleft > 0)
+        {
+            timeleft -= Time.deltaTime;
+        }
+
+        if (timeleft <= 0)
+        {
+            mode = mode + 1;
+            levelIsStarted = false;
+            counter = 0;
+            timeleft = 30.0f;
+        }
+      
+        
+    }
+
+
+
+
+    public void modesetup()
+    {
+        if (mode == 1)
+        {
+            text.GetComponent<TextMeshPro>().text = "Currently testing: \n Visual";
+        }
+        else if (mode == 2)
+        {
+            text.GetComponent<TextMeshPro>().text = "Currently testing: \n Audio";
+        }
+        else if (mode == 3)
+        {
+            text.GetComponent<TextMeshPro>().text = "Currently testing: \n Haptic";
+        }
+        else if (mode == 4)
+        {
+            text.GetComponent<TextMeshPro>().text = "Currently testing: \n All";
+        }
+        else if (mode == 5)
+        {
+            text.GetComponent<TextMeshPro>().text = "Currently testing: \n All";
+        }
+        else if (mode == 6)
+        {
+            Debug.Log("visual wrong: " + test1Wrong + "visual correct: " + test1Correct + "Audio wrong: " + test2Wrong + "Audio correct: " + test2Correct+
+                "haptic wrong: " + test3Wrong + "haptic correct: " + test3Correct + "all wrong: " + test4Wrong + "all correct: " + test4Correct+
+                "test5 wrong: " + test5Wrong + "test5 visualcorrect: " + test5CorrectVisual + "test5 audiocorrect: " + test5CorrectAudio + "test5 hapticCorrect: " +test5CorrectHaptic);
+        }
+        if (mode == 2||mode==4 ||mode==5)
+        {
+            audiozone.gameObject.SetActive(true);
+        }
+        else
+        {
+            audiozone.gameObject.SetActive(false);
+        }
     }
 }
